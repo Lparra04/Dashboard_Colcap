c94fc708b7da329ab9f0607b69a00f5882b8a9b7
José Pinzón <85970948+Jomapime@users.noreply.github.com>
2021-12-14 23:29:23 -0500

    Create Drop.py

---
+++ Drop.py
@@ -0,0 +1,60 @@
+import dash
+import pandas as pd
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+from dash import dash_table
+
+df = pd.read_excel("C:/Users/JosePinzon/Documents/FINTRADE/Python/Archivo2021.xlsx") #Cargar el archivo exportado anteriormente
+
+
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+  dcc.Dropdown(id='Emi', multi=True,
+  options=[{'label': x, 'value': x} for x in sorted(df.EMISOR.unique())],
+  clearable=True,
+  placeholder="Seleccione el emisor de la accion"),
+  
+  dcc.Dropdown(id='Nemo', multi=True,
+  options=[{'label': x, 'value': x} for x in sorted(df.EMISOR.unique())],
+  clearable=True,
+  placeholder="Seleccione el nemotecnico de la accion"),
+
+  html.Div(id='dd-output-container'),
+  
+  dash_table.DataTable(
+      id='Tabla',
+      columns=[{"name": i, "id": i} for i in df.columns],
+      data=df.to_dict('records'),
+      page_action="native",  #Permite actualizar con Fecha
+      page_size=20)
+      
+])
+
+
+@app.callback(
+    Output('Tabla', 'data'),
+    [Input('Emi', 'value'),
+    Input('Nemo', 'value')],
+    prevent_initial_call=False
+)
+
+def update_rows(selected_value,val_chosen):
+
+  if len(selected_value,) == 0:
+    data=df.to_dict('records')
+    return data
+  elif len(selected_value) > 0:
+    data = df[df['EMISOR'].isin(selected_value)].to_dict("records") #Aplicar el filtro sobre el df y la tabla
+    return data #retorne el df
+
+
+
+if __name__ == '__main__':
+    app.run_server()
+    
+    
+
