9f23e8b7f7956948fa2342ad93a69dccef2b0c9b
José Pinzón <85970948+Jomapime@users.noreply.github.com>
2022-01-25 23:54:53 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,16 +2,18 @@ library(shiny)
 library(shinydashboard)
 library(dplyr)
 library(shinythemes)
-library(shinydashboard)
 library(readxl)
 library(shinyWidgets)
+library(ggplot2)
 
-Archivo2020 <- read_excel("C:\\Users\\josep\\Documents\\Universidad\\FINTRADE\\Python\\Archivo2020.xlsx")
+Archivo2020 <- read_excel("C:/Users/josep/Documents/Universidad/FINTRADE/Python/Archivo2020.xlsx")
 Archivo2020$`FECHA ASAMBLEA` <- as.Date(Archivo2020$`FECHA ASAMBLEA`)
-ACCIONES <- read_excel("C:\\Users\\josep\\Documents\\Universidad\\FINTRADE\\Python\\ACCIONES.xlsx")
+Archivo2020$`FECHA INICIAL` <- as.Date(Archivo2020$`FECHA INICIAL`)
+ACCIONES <- read_excel("C:/Users/josep/Documents/Universidad/FINTRADE/Python/ACCIONES.xlsx")
 ACCIONES$fecha <- as.Date(ACCIONES$fecha)
-algo <- tidyr::gather(ACCIONES, key = "Accion", value = "Precio",
-                      PFBCOLOM, NUTRESA, PFGRUPSURA,ECOPETROL)
+
+df <- tidyr::gather(ACCIONES, key = "Accion", value = "Precio",
+                    PFBCOLOM, NUTRESA, PFGRUPSURA,ECOPETROL)
 
 opciones <- c("PFBCOLOM", "NUTRESA", "PFGRUPSURA","ECOPETROL")
 
@@ -26,9 +28,11 @@ ui <- dashboardPage(
     sidebarMenu(
       id = "Menu",
       style = "position: relative; overflow: visible;",
-      menuItem("Base de datos", tabName = "DataF", icon = icon("database", lib = "font-awesome")),
+      menuItem("Base de datos", tabName = "DataF", 
+               icon = icon("database", lib = "font-awesome")),
       
-      menuItem("Retorno Acciones", tabName = 'ACC', icon = icon("chart-line", lib = "font-awesome"))
+      menuItem("Retorno Acciones", tabName = 'ACC',
+               icon = icon("chart-line", lib = "font-awesome"))
     )
   ),
   dashboardBody(
@@ -41,15 +45,21 @@ ui <- dashboardPage(
                                 start = "2020-01-01",end = "2020-12-31",
                                 min = "2020-01-01",max = "2020-12-31",
                                 format = "yyyy-mm-dd",
-                                width = "200px")),
+                                width = "200px"),
+                 pickerInput("IN_Sector", label = "Sector Economico",
+                             choices = NULL, multiple = T, 
+                             options = list(`actions-box` = TRUE))),
           column(3,
                  pickerInput("IN_Emisor",label = "Emisor",
                              choices = NULL,multiple = T,
                              options = list(`actions-box` = TRUE)),
                  pickerInput("IN_Nemo",label = "Nemo",
-                             choices = NULL,multiple = T,options = list(`actions-box` = TRUE)),
+                             choices = NULL,multiple = T,
+                             options = list(`actions-box` = TRUE))
           )),
-        
+        h2(),
+        br(),
+        plotOutput("Grafico_AD"),
         h2("Base de datos fechas Ex-dividendo"),
         br(),
         fluidRow(
@@ -91,18 +101,28 @@ server <- function(input, output, session) {
   ################################## PRIMER MENU #################################
   
   observe({
-    dat0<-filter(Archivo2020,`FECHA ASAMBLEA` >= input$IN_Fechas[1] & `FECHA ASAMBLEA` <= input$IN_Fechas[2])
-    updatePickerInput(session, "IN_Emisor", label = "Emisor", choices = sort(unique(dat0$EMISOR)),selected = unique(dat0$EMISOR))
+    dat0<-filter(Archivo2020,`FECHA ASAMBLEA` >= input$IN_Fechas[1] &
+                   `FECHA ASAMBLEA` <= input$IN_Fechas[2])
+    updatePickerInput(session, "IN_Sector", label = "Sector economico", 
+                      choices = sort(unique(dat0$SECTOR)),selected = unique(dat0$SECTOR))
   })
   
   observe({
-    dat1<-Archivo2020$NEMOTÉCNICO[Archivo2020$EMISOR%in%input$IN_Emisor]
-    updatePickerInput(session, "IN_Nemo", label = "Nemotecnico", choices = sort(unique(dat1)),selected = unique(dat1))
+    dat1<-Archivo2020$EMISOR[Archivo2020$SECTOR%in%input$IN_Sector]
+    updatePickerInput(session, "IN_Emisor", label = "Emisor de la accion", 
+                      choices = sort(unique(dat1)),selected = unique(dat1))
+  })
+  
+  observe({
+    dat2<-Archivo2020$NEMOTÉCNICO[Archivo2020$EMISOR%in%input$IN_Emisor]
+    updatePickerInput(session, "IN_Nemo", label = "Nemotecnico", 
+                      choices = sort(unique(dat2)),selected = unique(dat2))
   })
   
   
   datn<-reactive({
-    Archivo2020 %>% filter(EMISOR %in% input$IN_Emisor &
+    Archivo2020 %>% filter(SECTOR %in% input$IN_Sector&
+                             EMISOR %in% input$IN_Emisor &
                              NEMOTÉCNICO %in% input$IN_Nemo)%>%
       select(-`FECHA INGRESO`, -`MONTO TOTAL ENTREGADO EN DIVIDENDOS`, -`FECHA INICIAL`)
   })
@@ -114,6 +134,34 @@ server <- function(input, output, session) {
   })
   
   
+  
+  Base_AD<-reactive({
+    Archivo2020%>%
+      group_by(`FECHA INICIAL`)%>%
+      filter(`FECHA ASAMBLEA` >= input$IN_Fechas[1] &
+               `FECHA ASAMBLEA` <= input$IN_Fechas[2] &
+               SECTOR %in% input$IN_Sector & 
+               EMISOR %in% input$IN_Emisor &
+               NEMOTÉCNICO %in% input$IN_Nemo)%>%
+      summarize(Total = sum(`VALOR CUOTA`))%>%
+      mutate(T_div=cumsum(Total))
+  })
+  
+  
+  
+  output$Grafico_AD <- renderPlot({
+    
+    ggplot(Base_AD(), aes(x=`FECHA INICIAL`,y=T_div))+
+      geom_step(colour = "Blue")+
+      scale_y_continuous(labels = scales::label_comma(), 
+                         breaks = scales::breaks_extended(n = 10))+
+      scale_x_date(date_breaks = "1 month",
+                   date_labels = "%b-%y")+
+      labs(title = "Grafico dividendos acumulados Colcap")
+    
+  })
+  
+  
   ################################# sEGUNDO MENU #################################
   
   
@@ -121,31 +169,30 @@ server <- function(input, output, session) {
     Archivo2020 %>% filter(NEMOTÉCNICO %in% input$IN_Nemo1 &
                              `FECHA ASAMBLEA` >= input$IN_Fechas1[1] & `FECHA ASAMBLEA` <= input$IN_Fechas1[2])%>%
       select(-`FECHA INGRESO`, -`MONTO TOTAL ENTREGADO EN DIVIDENDOS`, -`FECHA INICIAL`,
-             -`DESCRIPCIÓN PAGO PDU`,-MONEDA,-`VALOR TOTAL DEL DIVIDENDO`,-`FECHA FINAL Y DE PAGO`)
+             -`DESCRIPCIÃ“N PAGO PDU`,-MONEDA,-`VALOR TOTAL DEL DIVIDENDO`,-`FECHA FINAL Y DE PAGO`)
   })
   
+  
+  
   Retorno_Precio <- reactive({
-    filter(algo, Accion %in% input$IN_Nemo1 & 
-             fecha >= input$IN_Fechas1[1] & fecha <= input$IN_Fechas1[2]) 
-    
+    filter(df, Accion %in% input$IN_Nemo1 &
+             fecha >= input$IN_Fechas1[1] & fecha <= input$IN_Fechas1[2])
   })
   
   output$base1 <- renderPrint({
     
-    Buena <- Base_R()
-    sum(Buena$`VALOR CUOTA`)
+    Reac_b <- Base_R()
+    sum(Reac_b$`VALOR CUOTA`)
     
   })
   
   
   output$base2 <- renderPrint({
-    
-    Buena <- Retorno_Precio()
-    tail(Buena$Precio,1) - head(Buena$Precio,1)
-    
+    Reac_b <- Retorno_Precio()
+    tail(Reac_b$Precio,1) - head(Reac_b$Precio,1)
   })
   
-
+  
   
 }
 
